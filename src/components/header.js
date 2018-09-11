d1b8e7a306007b7ebb7be0527e041ae2dfcaae94
chayatep <chayatep01@hotmail.com>
2018-09-11 21:45:09 +0700

    :clown_face:Add Header

--- src/components/header.js
+++ src/components/header.js
@@ -1,9 +1,21 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import logo from '../img/logo.png'
 
 const Header = () => (
-  <div>
-    <h1>ร้านอาหารเจไทย</h1>
+  <div className="header">
+      <div className="logo-box">
+        <img src={logo} alt="Jthai-logo" className="logo"/>
+      </div>
+      <div className="text-box">
+      <h1 className="heading-primary">
+        <span className="heading-primary-main">ร้านอาหารเจไทย </span>
+        <span className="heading-primary-sub">Thai Vegeterian Resturant</span>
+      </h1>
+        <p>C o m i n g    s o o n .. </p>
+      </div>
+
+     
   </div>
 )
 
